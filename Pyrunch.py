6d7334e6ec54d854b3288fbdb0bfdc6a96b9a94a
amirreza es <46327286+amirrezaes@users.noreply.github.com>
2020-08-04 00:06:19 +0430

    Added Mask
    now you can make masks

--- Pyrunch.py
+++ Pyrunch.py
@@ -17,9 +17,14 @@ Options:
 
 '''
 
-start = time.time()
-n = 0  # we are using n to get number of generated combinations
+alpha_u = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alpha_l = 'abcdefghijklmnopqrstuvwxyz'
+numbers = '0123456789'
+symbol = """!@#$%^&*()_+-=,.`~[]\{}|;':"/<>?'"""
 
+start = time.time()
+
+n = 0  # we are using n to track the number of generated combinations
 workingpath = os.getcwd()
 WriteToFile = False
 Filename = None
@@ -27,6 +32,7 @@ MemoryFriendly = False
 algo = None
 Pre = ''
 Suf = ''
+mask = None
 
 
 def Generator(*mystring, Length, algo=None):  # func for generating combinations
@@ -34,11 +40,10 @@ def Generator(*mystring, Length, algo=None):  # func for generating combinations
     recurser = [tuple(iter) for iter in mystring] * Length
     for iter in recurser:
         result = (i+[j] for i in result for j in iter)
-    if algo is None:
-        yield from result
-    else:
+    if algo is not None:
         hashed = (ha.new(algo, data=''.join(i).encode()).hexdigest() for i in result)
         yield from hashed
+    yield from result
 
 
 def Output(Password, Pre, Suf):  # func for wrapping outputs
@@ -70,15 +75,56 @@ def Output(Password, Pre, Suf):  # func for wrapping outputs
                 out.writelines(chunk)
 
 
+def Gen_mask(mask):
+    signs = {'@': alpha_l, ',': alpha_u, '$': symbol, '%': numbers}
+    mask_index = {'@': [], '%': []}
+    item = 0
+    cut = 1
+    while item < len(mask):
+        if mask[item] == '@':
+            if not cut:
+                mask_index['@'][-1]+='@'
+                c = 0
+            elif cut:
+                mask_index['@'].append('@')
+                cut = 0
+        elif mask[item] == '%':
+            if not cut:
+                mask_index['%'][-1]+='%'
+                cut = 0
+            elif cut:
+                mask_index['%'].append('%')
+                cut = 0
+        else:
+            cut = 1
+        item+=1
+    item = [mask_index[i][j] for i in mask_index for j in range(0, len(mask_index[i]))]
+    result = [mask]
+    for i in item:
+        recurser = i
+        charset = signs[i[0]]
+        copy_result = result.copy()
+        result.clear()
+        for mask in copy_result:
+            for c in Generator(charset, Length=len(i)):
+                mask = mask.replace(recurser, ''.join(c), 1)
+                result.append(mask)
+                recurser = ''.join(c)
+    yield from result
+
+
 def parse(args):
     global WriteToFile, Filename, MemoryFriendly, algo, Pre, Suf, ha
-    global mystring, minlength, maxlength
+    global mystring, minlength, maxlength, mask
     if '-h' in args or '--help' in args or '-h' in sys.argv[1:2]:
         print(intro)
         sys.exit(0)
-    mystring = str(sys.argv[3])
-    minlength = int(sys.argv[1])
-    maxlength = int(sys.argv[2])
+    if '--mask' in sys.argv:
+        mask = sys.argv[sys.argv.index('--mask') + 1]
+    else:
+        mystring = str(sys.argv[3])
+        minlength = int(sys.argv[1])
+        maxlength = int(sys.argv[2])
     if args != []:
         arg = 0
         while arg < len(args):
@@ -111,7 +157,9 @@ def main():
     global start, all_pos
 
     start = time.time()
-    if maxlength == minlength:  # generating combinations with same length
+    if mask is not None:
+        Output(Gen_mask(mask), Pre, Suf)
+    elif maxlength == minlength:  # generating combinations with same length
         all_pos = len(mystring) ** minlength  # calculatinf all possible combs
         Output(Generator(mystring, Length=minlength, algo=algo), Pre, Suf)
     else:  # generating combinations with diffrent lengths
