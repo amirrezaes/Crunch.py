6e39d0e38751bf48db578f6d37617337035ef242
amirreza es <46327286+amirrezaes@users.noreply.github.com>
2020-08-28 01:08:43 +0430

    Update Pyrunch.py

--- Pyrunch.py
+++ Pyrunch.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import time
-
+import tkinter, tkinter.font as tkFont
 intro = '''
 Usage: pyrunch.py <min> <max> <characters> <options>
        or
@@ -22,11 +22,10 @@ Options:
    -h         Print This Help Message
 
 '''
-
-alpha_u = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alpha_l = 'abcdefghijklmnopqrstuvwxyz'
-numbers = '0123456789'
-symbol = """!@#$%^&*()_+-=,.`~[]\{}|;':"/<>?'"""
+chars = {'alpha_u': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+         'alpha_l': 'abcdefghijklmnopqrstuvwxyz',
+         'numbers': '0123456789',
+         'symbol': """!@#$%^&*()_+-=,.`~[]\{}|;':"/<>?"""}
 
 start = time.time()
 
@@ -39,7 +38,7 @@ algo = None
 Pre = ''
 Suf = ''
 mask = None
-
+gui = False
 
 def Generator(*mystring, Length, algo=None):  # func for generating combinations
     result = [[]]
@@ -52,17 +51,17 @@ def Generator(*mystring, Length, algo=None):  # func for generating combinations
     yield from result
 
 
-def Gen_mask(mask):
+def Gen_mask(mask):  # func for generating mask passwords
     result = [mask]
     for word in result:
-        if '@' in word:
-            result.extend([word.replace('@', i, 1) for i in alpha_l])
-        elif '%' in word:
-            result.extend([word.replace('%', i, 1) for i in numbers])
+        if '%' in word:
+            result.extend([word.replace('%', i, 1) for i in chars["numbers"]])
+        elif '@' in word:
+            result.extend([word.replace('@', i, 1) for i in chars["alpha_l"]])
         elif ',' in word:
-            result.extend([word.replace(',', i, 1) for i in alpha_u])
+            result.extend([word.replace(',', i, 1) for i in chars["alpha_u"]])
         elif '$' in word:
-            result.extend([word.replace('$', i, 1) for i in symbol])
+            result.extend([word.replace('$', i, 1) for i in chars["symbol"]])
     return result[-all_pos:]
 
 
@@ -78,7 +77,10 @@ def Output(Password, Pre, Suf):  # func for wrapping outputs
                     out.write(Pre+''.join(item)+Suf+'\n')
                     n+=1
                     if n % round(all_pos/100) == 0:
-                        print('Working: ', round((n*100)/all_pos), '%', end='\r')
+                        if gui:
+                            return round((n*100)/all_pos)
+                        else:
+                            print('Working: ', round((n*100)/all_pos), '%', end='\r')
                 return
             chunk = []  # we will store 1% of combs every time before writing
             for item in Password:
@@ -88,7 +90,10 @@ def Output(Password, Pre, Suf):  # func for wrapping outputs
                     if n % round(all_pos/100) == 0:
                         out.writelines(chunk)
                         chunk.clear()
-                        print('Working: ', round((n*100)/all_pos), '%', end='\r')
+                        if gui:
+                            return round((n*100)/all_pos)
+                        else:
+                            print('Working: ', round((n*100)/all_pos), '%', end='\r')
                 except ZeroDivisionError:  # if all_pos is less than 50 the round() will turn it to 0 and we will get this error
                     pass
             if len(chunk) > 0:
@@ -101,6 +106,11 @@ def parse(args):
     if '-h' in args or '--help' in args or '-h' in sys.argv[1:2]:
         print(intro)
         sys.exit(0)
+    if '--gui' in sys.argv:
+        gui = True
+        WriteToFile = True
+        Gui()
+        return
     if '--mask' in sys.argv:
         mask = sys.argv[sys.argv.index('--mask') + 1]
         args = sys.argv[2:]
@@ -133,6 +143,53 @@ def parse(args):
             arg += 1
 
 
+def Gui():
+    def start():
+        global minlength, maxlength, mystring, Filename
+        try:
+            minlength = int(minlen.get())
+            maxlength = int(maxlen.get())
+            Filename = dir.get()
+            if Filename == '':
+                Filename='passwords.txt'
+        except ValueError:
+            print("only numbers are acceptable for lengh")
+        mystring = ''
+        for checked in checks:
+            if checks[checked].get():
+                mystring += chars[checked]
+        graf.destroy()
+
+    graf = tkinter.Tk()
+    graf.geometry("450x250+500+200")
+    graf.title('Pyrunch')
+    graf.configure(background='black')
+    fontStyle= tkFont.Font(family="Lucida Gran", size=12)
+
+    checks ={
+        'numbers': tkinter.IntVar(),
+        'alpha_l': tkinter.IntVar(),
+        'alpha_u': tkinter.IntVar(),
+        'symbol': tkinter.IntVar()}
+
+    tkinter.Checkbutton(graf, text='Numbers', variable=checks['numbers'], bg='black', fg='green').grid(row='0', sticky='W')
+    tkinter.Checkbutton(graf, text='Lower Alphabet', variable=checks['alpha_l'], bg='black', fg='green').grid(row='1', sticky='W')
+    tkinter.Checkbutton(graf, text='Upper Alphabet', variable=checks['alpha_u'], bg='black', fg='green').grid(row='2', sticky='W')
+    tkinter.Checkbutton(graf, text='Special Characters', variable=checks['symbol'], bg='black', fg='green').grid(row='3', sticky='W')
+    minlen = tkinter.Entry(graf, width=8)
+    minlen.insert(0,'min')
+    minlen.place(x=162, y=120)
+    maxlen = tkinter.Entry(graf, width=8)
+    maxlen.insert(0,'max')
+    maxlen.place(x=218, y=120)
+    tkinter.Label(graf, text='File name:', bg='black', fg='red', font=fontStyle).place(x=200, y=10)
+    dir = tkinter.Entry(graf, width=25)
+    dir.place(x=280, y=10)
+    tkinter.Button(graf, text='Generate', command=start, bg='dark sea green').place(x=190, y=180)
+
+    graf.mainloop()
+
+
 def main():
     global start, all_pos
 
@@ -162,13 +219,12 @@ if __name__ == '__main__':
     try:
         parse(sys.argv[4:])
         main()
-    except (NameError, IndexError, ValueError) as e:
-        print(e)
-        print("Incorrect Arguments use -h or --help for help.")
+    #except (NameError, IndexError, ValueError):
+    #    print("Incorrect Arguments use -h or --help for help.")
     except KeyboardInterrupt:
-        print("Stoped")
+        print("\nStoped")
     except MemoryError:
         print("Memory is Full, Use Memory Friendly Mode")
     finally:
         end = time.time()
-        print('\n', 'Ended in: ', round(end-start, 10), 'sec')
+        print(f'\n Ended in: {round(end-start, 10)} sec')
